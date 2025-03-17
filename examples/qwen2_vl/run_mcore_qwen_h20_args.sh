a534c90543e59ac5e1f735a52d5a938f0d942412
searobbersduck <searobbersandduck@gmail.com>
2025-03-17 12:41:10 +0000

    commit

---
+++ examples/qwen2_vl/run_mcore_qwen_h20_args.sh
@@ -0,0 +1,27 @@
+sh run_mcore_qwen_h20.sh  \
+dsw  \
+7B   \
+1    \
+32 \
+1e-5   \
+1e-6   \
+2048  \
+2048  \
+bf16  \
+2   \
+2  \
+1 \
+false \
+false \
+true   \
+false \
+false \
+100000  \
+/workspace/data/mm/LLaVA-Pretrain/wds   \
+/workspace/data/mm/LLaVA-Pretrain/wds   \
+/workspace/data/mm/model/qwen2-vl-ckpts/Qwen2-VL-2B-Instruct \
+20000  \
+200   \
+./output_mcore_qwen2vl_pretrain \
+6 \
+0
